764363ae0d76e94ee7ba3d2f9298f62b2b698390
fatifleur0203-pixel <fatifleur0203@gmail.com>
2026-04-24 21:46:15 +0100

    Add basic arithmetic operations in script.js
    Fix arithmetic operations and division handling.

---
+++ script.js
@@ -0,0 +1,15 @@
+let n1=prompt("donner le nombre 1:");
+let n2=prompt("donner le nombre 2:");
+let somme = n1+n2;
+let difference = n1-n2;
+let produit = n1*n2;
+let division = n1/n2;
+if  (n2 !== 0) {
+    prompt("la division est :" /division);}
+else{
+    prompt("la division sur zero est impossible:");}
+console.log("somme:"+somme);
+console.log("difference:"-difference);
+console.log("produit:"*produit);
+console.log("quotient:"/quotirnt);
+    
